378df90c9883934d23c38973b75f818215a04745
DamianZ00 <damian.zyla@edu.uekat.pl>
2023-12-03 15:12:01 +0100

    usuniecie zadan z innej galezi

--- Exercise_2_C.py
+++
@@ -1,8 +0,0 @@
-def display_even_numbers(numbers):
-    for number in numbers:
-        if number % 2 == 0:
-            print(number)
-
-numbers_list = list(range(2, 21, 2))
-
-display_even_numbers(numbers_list)
--- Exercise_2_D.py
+++
@@ -1,7 +0,0 @@
-def display_every_second_element(numbers):
-    for i in range(0, len(numbers), 2):
-        print(numbers[i])
-
-numbers_list = list(range(1, 11))
-
-display_every_second_element(numbers_list)
