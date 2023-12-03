5145012e30660d55ab1c5074493cfb5560466de8
DamianZ00 <damian.zyla@edu.uekat.pl>
2023-12-03 15:41:02 +0100

    pep-8

--- Exercise_2_A.py
+++ Exercise_2_A.py
@@ -1,10 +1,8 @@
 def list_names(names):
     for name in names:
-        print (name)
+        print(name)
 
 
 ws_names = ['Jan', 'Bartek', 'Damian', 'Michał', 'Witold']
 
 list_names(ws_names)
-
-
--- Exercise_2_B.py
+++ Exercise_2_B.py
@@ -1,15 +1,15 @@
-#Wersja z pętlą for
+# Wersja z pętlą for
 def multiply_by_two_1(numbers):
     for number in numbers:
         print(number * 2)
 
 
-#Wersja z listą składaną
+# Wersja z listą składaną
 def multiply_by_two_2(numbers):
     print([i*2 for i in numbers])
 
 
-ws_numbers = [1,2,3,4,5]
+ws_numbers = [1, 2, 3, 4, 5]
 
 multiply_by_two_1(ws_numbers)
 multiply_by_two_2(ws_numbers)
